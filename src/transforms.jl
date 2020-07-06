ac8b41b9947413b1bf200448cf06e61a6440ef60
sapal6 <satyabrata.pal1@gmail.com>
2020-07-06 19:43:06 +0530

    added split functions to transforms

--- src/transforms.jl
+++ src/transforms.jl
@@ -21,35 +21,53 @@ Get, split, and label
     label them.
 =#
 
+using Unicode
 using MLDataUtils
+using DataStructures
 
 #TODO: ignore case while checking extesnions
-function process_files(path, files, extensions=nothing)
+function process_files(path::AbstractString,
+                       files::AbstractArray,
+                        extensions=nothing)
     #gets file as per an extension
-    #todo: create the actual fucntion
-    res = [joinpath(path, file) for file in files if !startswith(file, ".") && ((!extensions) || occursin(".$extensions", file))]
+      res = []
+      extensions === nothing ? res = [joinpath(path, file)
+                                    for file in files
+                                         if .!startswith(file, ".")] :
+                                      res = [joinpath(path, file) for file in files if .!startswith(file, ".") 
+                                      &&
+                                       occursin(extensions, Unicode.normalize(file, casefold=true))]
+      res
+end
+
+
+#todo: figure out what is teh role of the "folders" parameter in the original code
+function get_files(path::AbstractString,
+    extensions=nothing,
+    recurse::Bool=true)
+
+    if length(path) == 0
+        error("A path must be provided")
+    end
+
+    #Get all the files in path with optional extensions, optionally with recurse, only in folders, if specified.
+    res = AbstractString[]
+    file_names = []
+
+    if recurse
+       for (root, dirs, files) in walkdir(path)
+           for file in files
+               push!(file_names, joinpath(root,file))
+           end
+       end
+       res = process_files("", file_names, extensions)
+    else
+       files = [file for file in readdir(path, join=true) if isfile(file)]
+       res = process_files(path, files, extensions)
+    end
     res
 end
 
-#TODO: "folders"args is not used yet.
-function get_files(path::AbstractString,
-             extensions=nothing,
-             recurse::Bool=true,
-             folders=nothing)
-
-         """Get all the files in `path` with optional `extensions`, optionally with `recurse`, only in `folders`, if specified."""
-
-         if recurse
-            res = AbstractString[]
-            root, dirs, files = walkdir(path)
-            files = [joinpath(root,file) for file in files]
-            push!(res, process_files(path, files, extensions))
-         else
-            files = [file for file in readdir(path, join=true) if isfile(file)]
-            res = process_files(path, files, extensions)
-         end
-         res
-end
 
 #=
 Curry function that will provide the arguments first and then
@@ -65,10 +83,10 @@ filegetter("/test")
 =#
 
 
-function FileGetter(path, extensions=nothing, recurse=true, folders=nothing)
-    "Create `get_files` partial function that searches path suffix `suf`,
-     only in `folders`, if specified, and passes along args"
-     pathsuffix -> get_files(joinpath(path, pathsuffix),extensions, recurse, folders)
+function FileGetter(path, extensions=nothing, recurse=true)
+    #Create `get_files` partial function that searches path suffix `suf`,
+    #only in `folders`, if specified, and passes along args
+     pathsuffix -> get_files(joinpath(path, pathsuffix),extensions, recurse)
 end
 
 #=
@@ -76,8 +94,12 @@ end
 Convienience function to get images with standard image extension
 =#
 function get_image_files(path, recurse=true, folders=nothing)
-    image_extensions=Set{AbstractString}(["TIFF", "JPEG", "PNG", "GIF"])
-    get_files(path, image_extensions, recurse, folders)
+    res = []
+    image_extensions=Set{AbstractString}(["tiff", "jpeg", "png", "gif", "jpg"])
+    foreach(image_extensions) do img_extension
+        res = [file for file in get_files(path, img_extension, recurse)]
+    end
+    res
 end
 
 #=
@@ -100,7 +122,7 @@ Helper for text files
 =#
 function get_text_files(path, recurse=true, folders=nothing)
     "Get text files in `path` recursively, only in `folders`, if specified."
-    get_files(path, [".txt"], recurse, folders)
+    get_files(path, ".txt", recurse)
 end
 
 #=
@@ -134,3 +156,109 @@ class AttrGetter(ItemTransform):
     def __init__(self, nm, default=None): store_attr(self, 'nm,default')
     def encodes(self, x): return getattr(x, self.nm, self.default)
 =#
+
+
+#=
+Split
+The next set of functions are used to split data into training and validation sets.
+The functions return two lists - a list of indices or masks for each of training 
+and validation sets.
+
+These APIs cloesly mimic the original code's APIs.
+
+Functions from the MLDataUtils are leveraged here.
+=#
+
+#=Wrapper around the splitobs function from here -
+http://mldatapatternjl.readthedocs.io/en/latest/documentation/datasubset.html#split
+
+e.g. X = rand(2, 6)
+     split = RandomSplitter()
+     train, test = split(X)
+     --julia> train
+              2×4 SubArray{Float64,2,Array{Float64,2},Tuple{Colon,UnitRange{Int64}},true}:
+               0.226582  0.933372  0.505208   0.0443222
+               0.504629  0.522172  0.0997825  0.722906
+
+       julia> test
+       2×2 SubArray{Float64,2,Array{Float64,2},Tuple{Colon,UnitRange{Int64}},true}:
+        0.812814  0.11202
+        0.245457  0.000341996
+
+=#
+function RandomSplitter(valid_pct=0.2)
+    datatable -> splitobs(datatable, at = valid_pct);
+end
+
+#=
+Wrapper around stratifiedobs--
+http://mldatapatternjl.readthedocs.io/en/latest/documentation/targets.html#stratified
+
+Can be used for stratified partioning of data
+=#
+
+function TrainTestSplitter(data)
+     (args...) -> stratifiedobs(data, args...)
+end
+
+function TrainTestSplitter(f,data)
+    (args...) -> stratifiedobs(f,data, args...)
+end
+
+#=
+Split `items` so that `val_idx` are in the validation set 
+and the others in the training set
+=#
+function IndexSplitter(items::AbstractArray, valid_idx::AbstractArray)
+    train_idx= setdiff(SortedSet(items),SortedSet(valid_idx))
+    collect(train_idx), collect(valid_idx)
+end
+
+#=
+Return an array of indices of parent directories of files
+e.g. grandparent_idxs(["/folder/train/test.png", "/folder/valid/test2.png"],
+                      "train")
+[1]
+=#
+#the bool array is exactly len of actual array * len of names
+#have to find a way to find the true indices
+
+#TODO: code can be much cleaner
+function grandparent_idxs(items::AbstractArray, name::AbstractString)
+    truthvalues = [occursin("/$name/", item) for item in items]
+    idxs=findall(truthvalues)
+    idxs
+end
+#TODO: code can be much cleaner
+function grandparent_idxs(items::AbstractArray, names::Tuple)
+    truths=[]
+    for name in names
+        truthvalues = []
+        for item in items
+            push!(truthvalues,occursin("/$name/", item))
+        end
+        push!(truths, truthvalues)
+    end
+    idxs=[idx for truth in truths for idx in findall(truth)]
+    idxs
+end
+
+#=
+Split `items` from the grand parent folder names (`train_name` and `valid_name`).
+e.g. fnames -> GrandparentSplitter(train_name='train', valid_name='valid')
+[]
+=#
+function GrandparentSplitter()
+    train_idxs = items -> grandparent_idxs(items, "train")
+    valid_idxs = items -> grandparent_idxs(items, "valid")
+end
+
+function GrandparentSplitter(train_name::AbstractString, valid_name::AbstractString)
+    train_idxs = items -> grandparent_idxs(items, train_name)
+    valid_idxs = items -> grandparent_idxs(items, valid_name)
+end
+
+function GrandparentSplitter(train_name::Tuple, valid_name::Tuple)
+    train_idxs = items -> grandparent_idxs(items, train_name)
+    valid_idxs = items -> grandparent_idxs(items, valid_name)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,127 @@
 using FastAI2Julia
 using Test
 
-@testset "FastAI2Julia.jl" begin
-    # Write your tests here.
+include("../src/transforms.jl")
+
+macro excludeTest(func)
+    println("$func excluded from test run")
+    return nothing
+end
+
+macro includeTest(func)
+    return func
+end
+
+function test_transforms_process_files()
+    println("starting tests for process_files()...")
+    @test process_files(".", [".dataset.jl", "test.jl"], ".jl") == ["./test.jl"]
+    @test process_files(".", ["dataset.jl", "test.jl"], ".jl") == ["./dataset.jl", "./test.jl"]
+    @test process_files(".", ["dataset.jl", "test.jl"]) == ["./dataset.jl", "./test.jl"]
+    println("..End of tests for process_files(). Printing Summary...")
+end
+
+
+function test_transforms_get_files()
+    println("starting tests for process_files()...")
+    path = "test/test_folder"
+    extension = ".txt"
+    @test get_files(path,
+                    extension) == ["test/test_folder/test1/test1-1.txt",
+                                  "test/test_folder/test1/test1-2.txt",
+                                  "test/test_folder/test2/test2-1.txt",
+                                  "test/test_folder/test2/test2-2.txt"]
+    println("..End of tests for process_files(). Printing Summary...")
+
+    @test get_files(path) == ["test/test_folder/test1/test1-1.txt",
+                              "test/test_folder/test1/test1-2.txt",
+                              "test/test_folder/test1/test1-3.yml",
+                              "test/test_folder/test2/test2-1.txt",
+                              "test/test_folder/test2/test2-2.txt"]
+
+    @test get_files(path,".yml") == ["test/test_folder/test1/test1-3.yml"]
+end
+
+function test_Filegetter()
+    filegetter= FileGetter("test",".yml")
+    filegetter2 = FileGetter("",".yml")
+    @test filegetter("test_folder") == ["test/test_folder/test1/test1-3.yml"]
+    @test_throws ErrorException filegetter2("") == "A path must be provided"
+end
+
+function test_get_image_files()
+    @test get_image_files("test/test_img_folder") == ["test/test_img_folder/test.jpg"]
+end
+
+function test_Imagegetter()
+    imagegetter= FileGetter("test",".jpg")
+    imagegetter2 = FileGetter("",".jpg")
+    @test imagegetter("test_img_folder") == ["test/test_img_folder/test.jpg"]
+    @test_throws ErrorException imagegetter2("") == "A path must be provided"
+end
+
+function test_get_text_files()
+    @test get_text_files("test/test_folder") == ["test/test_folder/test1/test1-1.txt",
+                                                 "test/test_folder/test1/test1-2.txt",
+                                                 "test/test_folder/test2/test2-1.txt",
+                                                 "test/test_folder/test2/test2-2.txt"]
+end
+
+function test_Itemgetter()
+    data= [(5, 4), (4, 6)]
+    @test ItemGetter(1, data) == (5,4)
+    
+end
+
+function test_RandomSplitter()
+    X = rand(2, 6)
+    split = RandomSplitter()
+    train, test = split(X)
+    @test length(train) == length(X)-length(test)
+end
+
+function test_TrainTestSplitter()
+    y = [:a, :a, :a, :a, :a, :a, :b, :b, :b]
+    split = TrainTestSplitter(y)
+    train1,test1 = split(0.7)
+    @test length(train1) == length(y)-length(test1)
+end
+
+
+function test_TrainTestSplitter2()
+    X = [1 0; 1 0; 1 0; 1 0; 0 1; 0 1]
+    split = TrainTestSplitter(argmax, X)
+    train1,test1 = split(0.7)
+    @test length(train1) == length(X)-length(test1)
+end
+
+function test_IndexSplitter()
+    a,b = IndexSplitter([1,2,3,4,5], [3,5])
+    @test a == [1,2,4]
+    @test b == [3,5]
+end
+
+function test_grandparent_idxs()
+    files = ["test/test_folder1/test2/test1-1.txt",
+      "test/test_folder2/test1/test1-2.txt",
+      "test/test_folder/test2/test2-1.txt",
+      "test/test_folder/test2/test2-2.txt"]
+
+    @test grandparent_idxs(files, "test2") == [1,3,4]
+    @test grandparent_idxs(files, ("test1", "test2")) == [2,1,3,4]
+    
+end
+
+
+@testset "All" begin
+    @excludeTest test_transforms_process_files()
+    @excludeTest test_transforms_get_files()
+    @excludeTest test_Filegetter()
+    @excludeTest test_get_image_files()
+    @excludeTest test_Imagegetter()
+    @excludeTest test_get_text_files()
+    @excludeTest test_Itemgetter()
+    @excludeTest test_RandomSplitter()
+    @excludeTest test_TrainTestSplitter2()
+    @excludeTest test_IndexSplitter()
+    @includeTest test_grandparent_idxs()
 end
